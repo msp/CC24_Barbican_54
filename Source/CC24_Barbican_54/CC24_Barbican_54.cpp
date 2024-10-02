4bfaf386d5ea64587dbeb14f33a59364df90d503
Matt Spendlove <matt@cenatus.org>
2024-10-02 17:05:12 +0100

    Convert to C++ project to include the Dolby plugin in executable
    The stub is unused but it forces UE to do a C++ build!

---
+++ Source/CC24_Barbican_54/CC24_Barbican_54.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CC24_Barbican_54.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CC24_Barbican_54, "CC24_Barbican_54" );
